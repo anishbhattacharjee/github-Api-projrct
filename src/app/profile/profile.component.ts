4e20d2e7c18187d570f3073af48e72dc4b786539
anishbhattacharjee <anishbhattacharjee@gmail.com>
2019-08-07 17:43:18 +0530

    Add existing fils

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -11,27 +11,44 @@ import { map } from 'rxjs/operators';
 
 
 export class ProfileComponent implements OnInit {
-  users : any;
-  repos : any;
-  constructor(private profileservice : ProfileService) {
-    this.profileservice.getUser().subscribe( p => p);
-    this.profileservice.getAlluser().subscribe(p => {
-      this.users = p;
-    //  console.log(this.users);
-      
-    })
+  users: any;
+  repos: any;
+  values: any;
+  search: any;
+  username: string;
+  constructor(private profileservice: ProfileService) {
 
-  
+    this.profileservice.getUserlist().subscribe(p => {
+      this.users  = p ;
+     //console.log(this.users);
+
+    });
+
+
    }
 
   ngOnInit() {
-   
+
+
   }
+
+
+  onKey() {
+    this.profileservice.getSearchResult(this.username).subscribe(res => {
+
+      this.users = res.items;
+     // console.log(this.search);
+
+    } );
+
+
+  }
+
   showName(data){
-    console.log(data);
+    // console.log(data);
     this.profileservice.getRepos(data).subscribe(t => {
       this.repos = t;
-      console.log(this.repos);
+      // console.log(this.repos);
     });
 
   }
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -11,14 +11,14 @@ export class ProfileService {
   constructor(private httpClient: HttpClient) {
  
    }
-getAlluser(){
+getUserlist(){
   return this.httpClient.get("https://api.github.com/users").pipe(map(res => res ));
 }
-   getUser(){
 
-    return this.httpClient.get("https://api.github.com/search/users?q=anishbhattacharjee").pipe(map(res => res ));
-  }
   getRepos(d){
-    return this.httpClient.get("https://api.github.com/users/" + d +"/repos").pipe(map(res => res ));
+    return this.httpClient.get("https://api.github.com/users/" + d + '/repos').pipe(map(res => res ));
+  }
+  getSearchResult(data){
+    return this.httpClient.get("https://api.github.com/search/users?q=" + data).pipe(map(res => res ));
   }
 }
